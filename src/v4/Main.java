d4fa16214d204afbbd5e03defcf1e038978d1d9c
Lukas-Siller <sillerlukas4@gmail.com>
2021-04-20 10:20:01 +0200

    to test

--- src/v4/Main.java
+++ src/v4/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -25,10 +26,12 @@ public class Main extends Application {
         Button b1 = new Button("Add");
         Button b2 = new Button("Remove");
         Pane element = new Pane();
-        element.getStylesheets().add("v4/test.css");
+        //element.getStylesheets().add("v4/test.css");
+        element.setStyle("-fx-background-color: black");
         GridPane mainInputPane = new GridPane();
-        mainInputPane.add(b1, 0, 0, 2, 1);
+        mainInputPane.add(b1, 0, 0);
         mainInputPane.add(b2, 1, 0);
+
         column = 1;
         row = 1;
 
@@ -38,15 +41,20 @@ public class Main extends Application {
 
         //mit lambda ausdruck damit man meherer sachen durchführen kann
         b1.setOnAction(e ->{
-            mainInputPane.add(element, column, row);
+            System.out.println("Add");
+            mainInputPane.add(element, 2, 2);
 
             column++;
             if (column%4 == 0){
                 row++;
             }
 
-            mainInputPane.addColumn(column);
-            mainInputPane.addRow(row);
+            //mainInputPane.addColumn(column);
+            //mainInputPane.addRow(row);
+        });
+
+        b2.setOnAction(e -> {
+            System.out.println("remove");
         });
     }
 
