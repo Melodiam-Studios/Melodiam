81e0b5dc12e9514b4dced73f661894edc87bc5c0
Silas Demez <silasdemez@gmail.com>
2021-04-20 21:00:58 +0200

    finally added preview - effects not working

--- src/v4/Controller.java
+++ src/v4/Controller.java
@@ -1,17 +1,23 @@
 package v4;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Line;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Controller {
 
-    @FXML ImageView previewImage;
+    @FXML ImageView previewImage = new ImageView();
 
     @FXML
     public void initialize(){
@@ -20,35 +26,29 @@ public class Controller {
     public Pane createLine(int line_length, boolean needSchluessel){
         Pane pane = new Pane();
 
+        ArrayList<Line> lines = new ArrayList<>();
         Line l1 = new Line();
-        l1.setStartX(0);
-        l1.setStartY(10);
-        l1.setEndX(line_length);
-        l1.setEndY(10);
-
         Line l2 = new Line();
-        l2.setStartX(0);
-        l2.setStartY(20);
-        l2.setEndX(line_length);
-        l2.setEndY(20);
-
         Line l3 = new Line();
-        l3.setStartX(0);
-        l3.setStartY(30);
-        l3.setEndX(line_length);
-        l3.setEndY(30);
-
         Line l4 = new Line();
-        l4.setStartX(0);
-        l4.setStartY(40);
-        l4.setEndX(line_length);
-        l4.setEndY(40);
-
         Line l5 = new Line();
-        l5.setStartX(0);
-        l5.setStartY(50);
-        l5.setEndX(line_length);
-        l5.setEndY(50);
+
+        lines.add(l1);
+        lines.add(l2);
+        lines.add(l3);
+        lines.add(l4);
+        lines.add(l5);
+
+        int i = 10;
+
+        for (Line l:lines) {
+            l.setStartX(0);
+            l.setStartY(i);
+            l.setEndX(line_length);
+            l.setEndY(i);
+            i += 10;
+            pane.getChildren().add(l);
+        }
 
         Line l6 = new Line();
         l6.setStartX(line_length);
@@ -56,11 +56,6 @@ public class Controller {
         l6.setEndX(line_length);
         l6.setEndY(50);
 
-        pane.getChildren().add(l1);
-        pane.getChildren().add(l2);
-        pane.getChildren().add(l3);
-        pane.getChildren().add(l4);
-        pane.getChildren().add(l5);
         pane.getChildren().add(l6);
 
         if (needSchluessel){
@@ -73,13 +68,29 @@ public class Controller {
             notenSchlüssel.setY(-5);
         }
 
-        pane.setOnMousePressed(this::onMousePressed);
+        //pane.setOnMousePressed(this::onMousePressed);
+        //pane.setOnMouseMoved(this::onMouseMoved);
+
+
+        pane.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                Controller.this.onMousePressed(mouseEvent, pane);
+            }
+        });
+
+        pane.setOnMouseMoved(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                Controller.this.onMouseMoved(mouseEvent, pane);
+            }
+        });
 
         return pane;
 
     }
 
-    public void onMouseMoved(javafx.scene.input.MouseEvent mouseEvent) {
+    public void onMouseMoved(javafx.scene.input.MouseEvent mouseEvent, Pane pane) {
         System.out.println("Test");
 
         Point p = new Point();
@@ -90,27 +101,47 @@ public class Controller {
 
         Image image = new Image(getClass().getResource("/resources/bilder_noten/ViertelnoteUnten.png").toExternalForm());
 
-        ImageView hoveredImage = new ImageView();
+        previewImage.setImage(image);
+
+        //ImageView hoveredImage = new ImageView();
+
         try {
-            Pane stackPane = (Pane) mouseEvent.getSource();
-            stackPane.getChildren().add(hoveredImage);
+            //pane = (Pane) mouseEvent.getSource();
+            pane.getChildren().add(previewImage);
         }catch (Exception exception){
             System.out.println("You exactly touched a line");
-            Line stackPane = (Line) mouseEvent.getSource();
-            //stackPane.getChildren().add(hoveredImage);        // still needs to be done
         }
 
+        previewImage.setX(p.x);
+        previewImage.setY(p.y);
+        previewImage.setImage(image);
+        previewImage.setFitHeight(34);
+        previewImage.setFitWidth(11);
 
-        hoveredImage.setX(p.x);
-        hoveredImage.setY(p.y);
-        hoveredImage.setImage(image);
-        hoveredImage.setFitHeight(34);
-        hoveredImage.setFitWidth(11);
+        //Setting the preserve ratio of the image view
+        previewImage.setPreserveRatio(true);
+
+        //Instantiating the ColorAdjust class
+        ColorAdjust colorAdjust = new ColorAdjust();
+
+        //Setting the contrast value
+        colorAdjust.setContrast(0.2);
+
+        //Setting the hue value
+        colorAdjust.setHue(-0.05);
+
+        //Setting the brightness value
+        colorAdjust.setBrightness(0.2);
+
+        //Setting the saturation value
+        colorAdjust.setSaturation(0.8);
+
+        //Applying coloradjust effect to the ImageView node
+        previewImage.setEffect(colorAdjust);
 
     }
 
-    public void onMousePressed(javafx.scene.input.MouseEvent mouseEvent) {
-        System.out.println("Test");
+    public void onMousePressed(javafx.scene.input.MouseEvent mouseEvent, Pane pane) {
 
         Point p = new Point();
          p.x = (int) mouseEvent.getX()-5;
@@ -122,12 +153,10 @@ public class Controller {
 
         ImageView hoveredImage = new ImageView();
         try {
-            Pane stackPane = (Pane) mouseEvent.getSource();
-            stackPane.getChildren().add(hoveredImage);
+            //Pane pane = (Pane) mouseEvent.getSource();
+            pane.getChildren().add(hoveredImage);
         }catch (Exception exception){
             System.out.println("You exactly touched a line");
-            Line stackPane = (Line) mouseEvent.getSource();
-            //stackPane.getChildren().add(hoveredImage);        // still needs to be done
         }
 
 
@@ -136,18 +165,6 @@ public class Controller {
         hoveredImage.setImage(image);
         hoveredImage.setFitHeight(34);
         hoveredImage.setFitWidth(11);
-
-    }
-
-    public void onMouseMenuRequested(javafx.scene.input.MouseEvent mouseEvent) {
-        Image image = new Image(getClass().getResource("note.png").toExternalForm());
-        StackPane bo = (StackPane) mouseEvent.getSource();
-        System.out.println(bo);
-        ImageView hoveredImage = new ImageView();
-        hoveredImage.setImage(image);
-        //hoveredImage.fitHeightProperty();
-        hoveredImage.setFitHeight(34);
-        hoveredImage.setFitWidth(23);;
     }
 
 }
