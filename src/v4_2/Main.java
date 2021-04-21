e3beda75509061cf0686fbd0f0ccfc4ed0693f34
Lukas-Siller <sillerlukas4@gmail.com>
2021-04-21 11:01:56 +0200

    packegv4_2 hinzugefügt

--- src/v4_2/Main.java
+++ src/v4_2/Main.java
@@ -1,7 +1,6 @@
-package v4;
+package v4_2;
 
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
@@ -15,6 +14,7 @@ public class Main extends Application {
     private int column = 0;
 
     @Override
+    //versuch mit liste und visible
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("v2");
 
