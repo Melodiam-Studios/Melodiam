06191dc2c1fd927f5b85e12ce316380a98685435
Lukas-Siller <sillerlukas4mail.com>
2021-04-24 13:36:49 +0200

    Add und Remove funktionieren (in v4_2). Methoden dafür noch machen

--- src/v4_2/Main.java
+++ src/v4_2/Main.java
@@ -1,11 +1,16 @@
 package v4_2;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+
+import java.util.ArrayList;
 
 public class Main extends Application {
 
@@ -16,29 +21,96 @@ public class Main extends Application {
     //versuch mit liste und visible
     public void start(Stage primaryStage) throws Exception{
         // TO Do: making size to variable
-        //      : making remove
+        //      : add und remove in methode bringen
 
-        primaryStage.setTitle("v2");
+        GridPane root = new GridPane();
+        GridPane mainInputPane = new GridPane();
 
         Button b1 = new Button("Add");
         Button b2 = new Button("Remove");
         Pane element = new Pane();
         Controller controller = new Controller();
 
-        GridPane mainInputPane = new GridPane();
+        root.add(b1, 0, 0);
+        root.add(b2, 1, 0);
+        root.add(mainInputPane, 1,1);
 
-        mainInputPane.add(b1, 0, 0);
-        mainInputPane.add(b2, 1, 0);
+        ArrayList<Pane> storeLines = new ArrayList<>();         //alle Panes werden hier gespeichert und es werden dann immer die aufgerufen die es benötigt.
 
         column = 0;
-        row = 1;
+        row = 0;
 
-        primaryStage.setScene(new Scene(mainInputPane, 600  , 400));
+
+        Scene scene = new Scene(root, 600  , 400);
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.LEFT){
+                    if (column >= 0) column--;
+                    if (column < 0) {
+                        row--;
+                        column = 3;
+                    }
+
+                    System.out.println("Remove" + column + ", " + row);
+                    //storeLines.add(controller.createLine(100,true));
+                    storeLines.remove(storeLines.size()-1);
+                    drawPane(storeLines, mainInputPane);
+                }
+
+                if (event.getCode() == KeyCode.RIGHT){
+                    System.out.println("Addbefor" + column + ", " + row);
+
+                    if (column >= 4){
+                        row++;
+                        column = 0;
+
+                    }
+
+                    System.out.println("Add" + column + ", " + row);
+
+                    mainInputPane.addColumn(column);
+                    mainInputPane.addRow(row);
+
+                    //gibt den einzelnen zeilen eine fixe größe so das sie sich nicht verändern können
+                    RowConstraints rowWith = new RowConstraints(100);
+                    mainInputPane.getRowConstraints().add(rowWith);
+
+                    ColumnConstraints columnWith = new ColumnConstraints(100);
+                    mainInputPane.getColumnConstraints().add(columnWith);
+
+                    if (column == 0){
+                        storeLines.add(controller.createLine(100,true));
+                        drawPane(storeLines, mainInputPane);
+
+                        //mainInputPane.add(controller.createLine(100,true), column,row);
+                    }else{
+
+                        storeLines.add(controller.createLine(100,false));
+                        drawPane(storeLines, mainInputPane);
+                        //mainInputPane.add(controller.createLine(100,false), column,row);
+                    }
+                    column++;
+                }
+            }
+        });
+
+        primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.setTitle("v4_2");
+
 
 
         //mit lambda ausdruck damit man meherer sachen durchführen kann
         b1.setOnAction(e ->{
+
+            if (column%4 == 0){
+                row++;
+                column = 0;
+
+            }
+
             System.out.println(column);
             System.out.println(row);
             System.out.println("Add");
@@ -46,30 +118,26 @@ public class Main extends Application {
             mainInputPane.addColumn(column);
             mainInputPane.addRow(row);
 
+            //gibt den einzelnen zeilen eine fixe größe so das sie sich nicht verändern können
+            RowConstraints rowWith = new RowConstraints(100);
+            mainInputPane.getRowConstraints().add(rowWith);
+
+            ColumnConstraints columnWith = new ColumnConstraints(100);
+            mainInputPane.getColumnConstraints().add(columnWith);
+
             if (column == 0){
-                RowConstraints rowWith = new RowConstraints(100);       //gibt den einzelnen zeilen eine fixe größe so das sie sich nicht verändern können
-                mainInputPane.getRowConstraints().add(rowWith);
+                storeLines.add(controller.createLine(100,true));
+                drawPane(storeLines, mainInputPane);
 
-                ColumnConstraints columnWith = new ColumnConstraints(100);
-                mainInputPane.getColumnConstraints().add(columnWith);
-
-                mainInputPane.add(controller.createLine(100,true), column,row);
+                //mainInputPane.add(controller.createLine(100,true), column,row);
             }else{
-                RowConstraints rowWith = new RowConstraints(100);
-                mainInputPane.getRowConstraints().add(rowWith);
 
-                ColumnConstraints columnWith = new ColumnConstraints(100);
-                mainInputPane.getColumnConstraints().add(columnWith);
-
-                mainInputPane.add(controller.createLine(100,false), column,row);
+                storeLines.add(controller.createLine(100,false));
+                drawPane(storeLines, mainInputPane);
+                //mainInputPane.add(controller.createLine(100,false), column,row);
             }
 
             column++;
-            if (column%4 == 0){
-                row++;
-                column = 0;
-
-            }
 
         });
 
@@ -81,8 +149,28 @@ public class Main extends Application {
             }
 
             System.out.println("Remove" + column + ", " + row);
-            mainInputPane.getChildren().remove(column, row);
+            //storeLines.add(controller.createLine(100,true));
+            storeLines.remove(storeLines.size()-1);
+            drawPane(storeLines, mainInputPane);
+            //mainInputPane.getChildren().remove(column, row);
         });
+    }
+
+    //panes in grid pane geben
+    private void drawPane(ArrayList<Pane> arr, GridPane root) {
+        int column = 0, row = 0;
+
+        root.getChildren().clear();
+        for(Pane line : arr){
+            root.add(line, column, row);
+
+            column++;
+            if (column%4 == 0){
+                row++;
+                column = 0;
+
+            }
+        }
     }
 
 
