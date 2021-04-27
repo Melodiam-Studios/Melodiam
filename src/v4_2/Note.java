7c262acf80252049eae2adf5d30b4d9d12876795
Alex-Hofer <alex.hofer_bx@snets.it>
2021-04-27 10:11:38 +0200

    Classes Note and Notenblatt

---
+++ src/v4_2/Note.java
@@ -0,0 +1,10 @@
+package v4_2;
+
+public class Note {
+
+    double notenwert;
+    int vorzeichen;
+    int notenlaenge;
+    int oktavhoehe;
+
+}
---
+++ src/v4_2/Notenblatt.java
@@ -0,0 +1,12 @@
+package v4_2;
+
+import javafx.scene.shape.Line;
+
+import java.util.ArrayList;
+
+public class Notenblatt {
+    ArrayList<Takt> takte = new ArrayList<>();
+
+    
+
+}
