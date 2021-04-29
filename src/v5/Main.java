6923499765a01892bc62efc684e9f1ba14dd2c15
Lukas-Siller <sillerlukas4@gmail.com>
2021-04-29 11:19:56 +0200

    v5 hinzugefügt

---
+++ src/v5/Main.java
@@ -0,0 +1,146 @@
+package v5;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
+import javafx.stage.Stage;
+
+import java.sql.BatchUpdateException;
+import java.util.ArrayList;
+
+
+public class Main extends Application {
+
+    @FXML
+    ScrollPane scrollP;
+
+    private int row = 0;
+    private int column = 0;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        /*Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        GridPane mainInputPane = new GridPane();
+        Controller controller = new Controller();
+        Button b1 = new Button();
+
+        mainInputPane.addColumn(column);
+        mainInputPane.addRow(row);
+        Pane s = new Pane();
+        mainInputPane.add(b1 ,0, 0);
+
+        scrollP.setContent(mainInputPane);
+
+        ArrayList<Pane> storeLines = new ArrayList<>();         //alle Panes werden hier gespeichert und es werden dann immer die aufgerufen die es benötigt.
+
+        column = 0;
+        row = 0;
+
+
+        Scene scene = new Scene(root, 600  , 400);
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.LEFT){
+                    if (column >= 0) column--;
+                    if (column < 0) {
+                        row--;
+                        column = 3;
+                    }
+
+                    System.out.println("Remove" + column + ", " + row);
+                    //storeLines.add(controller.createLine(100,true));
+                    storeLines.remove(storeLines.size()-1);
+                    drawPane(storeLines, mainInputPane);
+                }
+
+                if (event.getCode() == KeyCode.RIGHT){
+                    System.out.println("Addbefor" + column + ", " + row);
+
+                    if (column >= 4){
+                        row++;
+                        column = 0;
+
+                    }
+
+                    System.out.println("Add" + column + ", " + row);
+
+                    mainInputPane.addColumn(column);
+                    mainInputPane.addRow(row);
+
+                    //gibt den einzelnen zeilen eine fixe größe so das sie sich nicht verändern können
+                    RowConstraints rowWith = new RowConstraints(100);
+                    mainInputPane.getRowConstraints().add(rowWith);
+
+                    ColumnConstraints columnWith = new ColumnConstraints(100);
+                    mainInputPane.getColumnConstraints().add(columnWith);
+
+                    if (column == 0){
+                        storeLines.add(controller.createLine(100,true));
+                        drawPane(storeLines, mainInputPane);
+
+                        //mainInputPane.add(controller.createLine(100,true), column,row);
+                    }else{
+
+                        storeLines.add(controller.createLine(100,false));
+                        drawPane(storeLines, mainInputPane);
+                        //mainInputPane.add(controller.createLine(100,false), column,row);
+                    }
+                    column++;
+                }
+            }
+        });
+
+
+        primaryStage.setTitle("Melodiam");
+        primaryStage.setScene(scene);
+        primaryStage.setMaximized(true);
+        primaryStage.show();*/
+
+        Controller c1 = new Controller();
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Melodiam");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMaximized(true);
+        primaryStage.show();
+
+        c1.initialize();
+
+
+    }
+
+    private void drawPane(ArrayList<Pane> arr, GridPane root) {
+        int column = 0, row = 0;
+
+        root.getChildren().clear();
+        for (Pane line : arr) {
+            root.add(line, column, row);
+
+            column++;
+            if (column % 4 == 0) {
+                row++;
+                column = 0;
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
