18690c5f9539ec4c459d69c3feb02b19fbd9954f
Lukas-Siller <sillerlukas4mail.com>
2021-05-01 12:12:34 +0200

    v5 funktioniert

--- src/v5/Controller.java
+++ src/v5/Controller.java
@@ -6,7 +6,6 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
