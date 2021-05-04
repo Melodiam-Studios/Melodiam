602214cc81e9aca854c650bb2754c1df3679f10c
Gavaii <61867970+Gavaii@users.noreply.github.com>
2021-05-04 11:19:43 +0200

    GUI-THEME

--- src/vL/Main.java
+++ src/vL/Main.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import jfxtras.styles.jmetro.JMetro;
+import jfxtras.styles.jmetro.Style;
+import v5.Controller;
 
 import java.util.Objects;
 
@@ -14,11 +17,14 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
         primaryStage.setTitle("Melodiam");
-        primaryStage.setScene(new Scene(root));
-        root.getStylesheets().add(Objects.requireNonNull(getClass()
-                .getResource("style.css")).toExternalForm());
+        JMetro jMetro=new JMetro(Style.LIGHT);
+        Scene scene = new Scene(root);
+
+        jMetro.setScene(scene);
+        primaryStage.setScene(jMetro.getScene());
         primaryStage.setMaximized(true);
         primaryStage.show();
+
     }
 
 
