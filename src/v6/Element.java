a2ddb1a4a5a02e2cdc33bb577f3e9a159849f9f7
Lukas-Siller <sillerlukas4@gmail.com>
2021-05-13 10:48:41 +0200

    v5 in v6

---
+++ src/v6/Element.java
@@ -0,0 +1,14 @@
+package v6;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public abstract class Element {
+    @FXML
+    protected Image img;
+
+    @FXML
+    protected ImageView imageView = new ImageView();
+
+}
