e989756262981666d2c55841d5f289ed220ca694
Gavaii <61867970+Gavaii@users.noreply.github.com>
2021-05-13 11:01:36 +0200

    Revert "Multithread- Musik abspielen"
    This reverts commit f76acf85084ac89a3ae0bb6dcaae42c9c403a5e3.

--- src/audio/PlayMelody.java
+++ src/audio/PlayMelody.java
@@ -1,25 +1,25 @@
 package audio;
 
 import org.jfugue.player.Player;
-import v5.Note;
 
-import java.util.ArrayList;
+public class PlayMelody {
 
-public class PlayMelody implements Runnable{
-    Integer takte;
-    int tempo;
-    String instrument;
-    public PlayMelody(int takte, int tempo, String instrument){
-        this.takte=takte;
-        this.tempo=tempo;
-        this.instrument=instrument;
+    public void start() {
+        Player player=new Player();
+
+
+        //https://objectcomputing.com/resources/publications/sett/january-2008-writing-music-in-java-two-approaches
+
+        player.play(
+                // "Itsy, bitsy spider, climbed up the water spout."
+                "i[Electric_Bass_Finger] T60 71w 71w 70w 72w A5q. A5q A5i G5q F5i G5q A5i F5q. Rq. " +
+                        // "Down came the rain and washed the spider out."
+                        "A5q. A5q Bb5i C6q. C6q. Bb5q A5i Bb5q C6i A5q. Rq. " +
+                        // "Out came the sun and dried up all the rain, so the"
+                        "F5q. F5q G5i A5q. A5q. G5q F5i G5q A5i F5q. C5q C5i " +
+                        // "itsy, bitsy spider went up the spout again."
+                        "F5q F5i F5q G5i A5q. A5q A5i G5q F5i G5q A5i F5q. Rq.");
+
     }
 
-
-    @Override
-    public void run() {
-        Player player = new Player();
-        player.play(takte.toString()+"w");
-
-    }
 }
