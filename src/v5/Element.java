610113669a92c31b9f34bf2e544ca6b34f1ea43f
Silas Demez <silasdemez@gmail.com>
2021-05-13 10:27:05 +0200

    javadoc in Element.java

--- src/v5/Element.java
+++ src/v5/Element.java
@@ -4,10 +4,19 @@ import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/**
+ * Element makes a generic type that combines {@link Note} and {@link Pause}
+ */
 public abstract class Element {
+    /**
+     * Image to display the Note/Pause
+     */
     @FXML
     protected Image img;
 
+    /**
+     * ImageView to display the Note/Pause
+     */
     @FXML
     protected ImageView imageView = new ImageView();
 
