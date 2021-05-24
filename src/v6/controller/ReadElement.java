06ed1988d22c1375b9ddbc688613d42a59ad1794
Silas Demez <silasdemez@gmail.com>
2021-05-24 11:14:59 +0200

    ReadElement aktualisiert

--- src/v6/controller/ReadElement.java
+++ src/v6/controller/ReadElement.java
@@ -14,18 +14,15 @@ public class ReadElement {
 
     protected double vorzeichenViewY;
 
-    protected int tonLeiter;
-
     protected int vorzeichen;
 
-    public ReadElement(int inTakt, int position, double imageViewX, double imageViewY, double vorzeichenViewX, double vorzeichenViewY, int tonLeiter, int vorzeichen) {
+    public ReadElement(int inTakt, int position, double imageViewX, double imageViewY, double vorzeichenViewX, double vorzeichenViewY, int vorzeichen) {
         this.inTakt = inTakt;
         this.position = position;
         this.imageViewX = imageViewX;
         this.imageViewY = imageViewY;
         this.vorzeichenViewX = vorzeichenViewX;
         this.vorzeichenViewY = vorzeichenViewY;
-        this.tonLeiter = tonLeiter;
         this.vorzeichen = vorzeichen;
     }
 }
